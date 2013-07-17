7f1e6ef360d7f83f3176c3dd72aa606a71213d42
dzucconi <mail@damonzucconi.com>
2013-07-17 12:53:26 -0400

    Comment clarifying the usage of Flaw

--- lib/lcl/types/flaw.rb
+++ lib/lcl/types/flaw.rb
@@ -1,6 +1,10 @@
 # Flaw
 #
 # A fault or imperfection associated with another entity.
+#
+# Implements an intialize method such that when included in a class,
+# every time an instance of that class is created there is a 50/50 chance
+# that each instance method will be undefined.
 module Flaw
   def initialize
     singleton = class << self; self end
