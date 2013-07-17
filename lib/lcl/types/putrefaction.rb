af03e5e41fb4c614c4120f3fa787f9ff1d0db1c6
dzucconi <mail@damonzucconi.com>
2013-07-17 12:30:06 -0400

    Demonstrate

--- lib/lcl/types/putrefaction.rb
+++ lib/lcl/types/putrefaction.rb
@@ -1,6 +1,10 @@
 # Putrefaction
 #
 # An entity that only produces its data when deleted (garbage collected).
+#
+# => putrefaction = Putrefaction.new
+# => exit
+#   => <Putrefaction:0x007f93d4b1d428>
 class Putrefaction
   def initialize
     ObjectSpace.define_finalizer(self, self.class.finalize(self))
