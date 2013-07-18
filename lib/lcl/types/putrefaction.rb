d204e895b70dab919d3b4e7993d8c2037ff31011
dzucconi <mail@damonzucconi.com>
2013-07-17 22:07:25 -0400

    Allow information to be stored; privatize inspect

--- lib/lcl/types/putrefaction.rb
+++ lib/lcl/types/putrefaction.rb
@@ -2,15 +2,21 @@
 #
 # An entity that only produces its data when deleted (garbage collected).
 #
-# => putrefaction = Putrefaction.new
+# => putrefaction = Putrefaction.new("information")
+#   => (Object doesn't support #inspect)
+#   =>
 # => exit
-#   => <Putrefaction:0x007f93d4b1d428>
+#   => #<Putrefaction:0x007fb7531a13e8 @__object__="information">
 class Putrefaction
-  def initialize
+  def initialize(object)
+    @__object__ = object
+
     ObjectSpace.define_finalizer(self, self.class.finalize(self))
   end
 
   def self.finalize(_self)
-    proc { STDOUT.write(_self.inspect) }
+    proc { STDOUT.write(_self.send(:inspect)) }
   end
+
+  private :inspect
 end
