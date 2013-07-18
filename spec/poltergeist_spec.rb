bb39bb139cdaddf46acefdd6605ae2e43774ea0a
dzucconi <mail@damonzucconi.com>
2013-07-18 13:12:54 -0400

    Spec for checking adding methods at runtime

--- spec/poltergeist_spec.rb
+++ spec/poltergeist_spec.rb
@@ -21,4 +21,15 @@ describe "Poltergeist" do
   it "should be identitcal to the initial" do
     @poltergeist.should eq(@initial)
   end
+
+  it "should be able to have methods added to it's class definition" do
+    @poltergeist.instance_eval { def foo; "bar"; end } # Adds method to String, not Poltergeist
+    @poltergeist.foo.should eq("bar")
+    @initial.foo.should eq("bar")
+
+    class Poltergeist; def bar; "foo"; end; end
+    @_poltergeist = Poltergeist.new(@initial)
+    @_poltergeist.bar.should eq("foo")
+    expect { @initial.bar }.to raise_error(NoMethodError)
+  end
 end
