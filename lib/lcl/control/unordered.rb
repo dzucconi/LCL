5050a4be62a3ff8e04f768a26559ca9436a533ac
dzucconi <mail@damonzucconi.com>
2013-07-22 10:22:20 -0400

    =begin =end style comment blocks

--- lib/lcl/control/unordered.rb
+++ lib/lcl/control/unordered.rb
@@ -4,21 +4,22 @@
 #
 # Executes a set of statements out of sequence.
 #
-# # =>
-#   unordered(*("a".."z").collect.with_index do |_, i|
-#     -> { STDOUT.write("#{_}#{i}") }
-#   end)
-#   => w22s18e4n13g6q16t19b1c2h7j9x23y24v21p15o14d3a0l11m12z25k10i8f5r17u20
-#
-# # =>
-#   x = 0
-#   unordered(*[
-#     -> { x = 1 },
-#     -> { x = 2 },
-#     -> { x = 3 }
-#   ])
-#   x
-#   => 1 || 2 || 3
+=begin
+unordered(*("a".."z").collect.with_index do |_, i|
+  -> { STDOUT.write("#{_}#{i}") }
+end)
+# => w22s18e4n13g6q16t19b1c2h7j9x23y24v21p15o14d3a0l11m12z25k10i8f5r17u20
+=end
+=begin
+  x = 0
+  unordered(*[
+    -> { x = 1 },
+    -> { x = 2 },
+    -> { x = 3 }
+  ])
+  x
+=end
+# => 1 || 2 || 3
 def unordered(*λ)
   λ.shuffle.each(&:call)
 end
