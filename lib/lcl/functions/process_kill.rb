d83c722df4700677b5fdc06a4d402c8e733cc890
dzucconi <mail@damonzucconi.com>
2013-07-22 11:49:07 -0400

    Break into two methods for testing

--- lib/lcl/functions/process_kill.rb
+++ lib/lcl/functions/process_kill.rb
@@ -1,12 +1,16 @@
 # process_kill
 #
 # Selects a process at random and kills it.
+def random_process
+  raise NotImplementedError unless os == :macosx
+
+  `ps -e`.scan(/\n[\s|\d]*\d*\s/).collect do |pid|
+    pid.gsub("\\n", "").strip
+  end.sample
+end
+
 def process_kill
   raise NotImplementedError unless os == :macosx
 
-  pids = `ps -e`.scan(/\n[\s|\d]*\d*\s/).collect do |pid|
-    pid.gsub("\\n", "").strip
-  end
-
-  `kill #{pids.sample}`
+  `kill #{random_process}`
 end
