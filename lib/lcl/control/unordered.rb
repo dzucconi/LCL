ac38d5979d4eca96937ee8f99dd3ad56b6c97695
dzucconi <mail@damonzucconi.com>
2013-07-22 11:48:57 -0400

    Clean up

--- lib/lcl/control/unordered.rb
+++ lib/lcl/control/unordered.rb
@@ -5,10 +5,10 @@
 # Executes a set of statements out of sequence.
 #
 =begin
-unordered(*("a".."z").collect.with_index do |_, i|
-  -> { STDOUT.write("#{_}#{i}") }
-end)
-# => w22s18e4n13g6q16t19b1c2h7j9x23y24v21p15o14d3a0l11m12z25k10i8f5r17u20
+  unordered(*("a".."z").collect.with_index do |_, i|
+    -> { STDOUT.write("#{_}#{i}") }
+  end)
+  # => w22s18e4n13g6q16t19b1c2h7j9x23y24v21p15o14d3a0l11m12z25k10i8f5r17u20
 =end
 =begin
   x = 0
@@ -18,8 +18,8 @@ end)
     -> { x = 3 }
   ])
   x
+  # => 1 || 2 || 3
 =end
-# => 1 || 2 || 3
 def unordered(*λ)
   λ.shuffle.each(&:call)
 end
